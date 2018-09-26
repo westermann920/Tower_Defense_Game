8ca7349c3ef4f896b6809b26bee6859c89dfde64
westermann920 <42929525+westermann920@users.noreply.github.com>
2018-09-26 18:34:05 +0200

    Added tower

--- Tower Defense Game/Assets/Scripts/Attack.cs
+++ Tower Defense Game/Assets/Scripts/Attack.cs
@@ -4,17 +4,56 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour {
 
+    private Transform target;
+    public float range = 10f;
+    public string enemyTag = "Enemy";
+
+    public Transform TowerRotation;
+
 	// Use this for initialization
 	void Start () {
-		
+        InvokeRepeating("FindTarget", 0f, 0.5f);
 	}
-	
+
+    void FindTarget() {
+        GameObject[] allEnemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+
+        foreach (GameObject enemy in allEnemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+            //EnemySpecs specs = enemy.GetComponent<EnemySpecs>();
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+    }
+
 	// Update is called once per frame
 	void Update () {
-        GameObject[] allEnemies = GameObject.FindGameObjectsWithTag("enemy");
-        foreach (var e in allEnemies)
+        if (target == null)
         {
-            EnemySpecs specs = e.GetComponent<EnemySpecs>();
+            return;
         }
+
+        Vector3 dirToEnemy = target.position - transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(dirToEnemy);
+        Vector3 rotation = targetRotation.eulerAngles;
+        TowerRotation.rotation = Quaternion.Euler (0f, rotation.y, 0f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, range);
     }
 }
